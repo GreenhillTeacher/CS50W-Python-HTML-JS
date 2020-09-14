105776d1ccbbe89dbcc4bf0e6e5595e39a596ee8
GreenhillTeacher <suarezm@greenhill.org>
2020-09-14 11:39:37 -0500

    class programs 09/14/2020

--- addingNumbers.py
+++ addingNumbers.py
@@ -1,5 +1,12 @@
 # Maria Suarez
-x= 3
-y= 7
-y=3+x
-print(y)
+x= 45
+y= 7.0 #float
+print(type(y))
+#y=3.0+x
+b=False
+#print(type(x))
+#print(type(b))
+print(type(y))
+print(int(4.9999))
+x =2
+    print
---
+++ looping.py
@@ -0,0 +1,6 @@
+print("1 2 3 4 5 6 7 8 9")
+
+print("1\n"+"2\n"+"3\n"+"4\n")
+print("1\n")
+print("2 2\n")
+print("3 3 3\n" )
